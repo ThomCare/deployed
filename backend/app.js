639ad57b3d56fc8ff174ca2189ccfef1612e2665
Miguel Ybera <josemiguel.ybera.iics@ust.edu.ph>
2021-09-17 09:37:17 +0800

    commit

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser')
+const path = require('path')
 
 const errorMiddleware = require('./middlewares/errors');
+
+// Setting up config file
+if(process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
 
 app.use(express.json());
 app.use(cookieParser());
@@ -27,6 +31,13 @@ app.use('/api/v1', audit)
 app.use('/api/v1', announcementType)
 app.use('/api/v1', course)
 app.use('/api/v1', form)
+
+if(process.env.NODE_ENV === 'PRODUCTION'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+    })
+}
 
 // Middleware to handle errors
 app.use(errorMiddleware);
