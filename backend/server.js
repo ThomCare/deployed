3ca8fba0a01f7c1773f994cf9dcd2e6d62b7adb3
Miguel Ybera <josemiguel.ybera.iics@ust.edu.ph>
2021-09-17 11:30:17 +0800

    update

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,72 @@ cloudinary.config({
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
 })
+//socket
+
+
+const socketio = require('socket.io')
+
+var cors = require('cors')
+app.use(cors())
+
+
+
+
+const io = socketio(server, {
+    cors: {
+        origin: 'https://thomcare.herokuapp.com',
+        methods: ["GET", "POST"],
+        credentials: true // ip add of frontend
+    }
+})
+
+
+//socket code
+let users = []
+
+const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) &&
+        users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+};
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id)
+
+    //take userID and socketId from user
+    socket.on('addUser', (userId) => {
+        addUser(userId, socket.id)
+        io.emit('getUsers', users)
+    })
+
+    //send and get message
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId);
+        try {
+            io.to(user.socketId).emit("getMessage", {
+                senderId,
+                text
+            });
+        } catch (err) {
+            console.log('user is offline')
+        }
+    });
+
+    //on disconnection
+    socket.on('disconnect', () => {
+        console.log('a user disconnected', socket.id)
+        removeUser(socket.id)
+        io.emit("getUsers", users);
+    })
+})
+
 
 // Handle Unhandled Promise Rejections
 process.on('unhandledRejection', err => {
