807186a63d2813b8bac358e6bfaa7a016741a004
Miguel Ybera <josemiguel.ybera.iics@ust.edu.ph>
2021-09-29 01:18:39 +0800

    testing lang

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -232,13 +232,13 @@ exports.verifyStudent = catchAsyncErrors(async (req, res, next) => {
 
 // Get currently logged in user details => /api/v1/me
 exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
-    const user = await User.findById(req.user.id)
+    //const user = await User.findById(req.user.id)
 
     //console.log('Current user:', user.firstName)
 
     res.status(200).json({
-        success: true,
-        user
+        success: true
+        //user
     })
 })
 
