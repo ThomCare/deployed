3f23becdf9c6fe9e3987bab9f1484a02d39ff5d3
Miguel Ybera <josemiguel.ybera.iics@ust.edu.ph>
2021-09-29 00:46:53 +0800

    asdasd

--- frontend/src/components/home/auth/Register.js
+++ frontend/src/components/home/auth/Register.js
@@ -9,17 +9,16 @@ import ConfirmRegister from './ConfirmRegister'
 const Register = ({ history }) => {
     const dispatch = useDispatch()
 
-    const { studentInfo } = useSelector(state => state.student)
-
+    
     const [user, setUser] = useState({
-        firstName: studentInfo ? studentInfo.firstName : '',
-        middleName: studentInfo ? studentInfo.middleName : '',
-        lastName: studentInfo ? studentInfo.lastName : '',
-        studentNumber: studentInfo ? studentInfo.studentNumber : '',
-        email: studentInfo ? studentInfo.email : '',
-        course: studentInfo ? studentInfo.course : '',
-        password: studentInfo ? studentInfo.password : '',
-        confirmPassword: studentInfo ? studentInfo.confirmPassword : ''
+        firstName: '',
+        middleName: '',
+        lastName: '',
+        studentNumber: '',
+        email: '',
+        course: '',
+        password: '',
+        confirmPassword: ''
     })
 
     const [submitted, setSubmitted] = useState(false)
