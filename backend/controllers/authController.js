9ac2ae445d9deeee47d62961cb1324eed3937231
Miguel Ybera <josemiguel.ybera.iics@ust.edu.ph>
2021-10-08 21:24:52 +0800

    updated protocol

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -108,7 +108,7 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
     await user.save({ validateBeforeSave: false })
 
     // create reset password url
-    const link = `${req.protocol}://${req.get('host')}/password/reset/${resetToken}`
+    const link = `https://${req.get('host')}/password/reset/${resetToken}`
 
     try {
         const message = await resetPassword({ link })
@@ -186,7 +186,7 @@ exports.registerStudent = catchAsyncErrors(async (req, res, next) => {
     const registerToken = jwt.sign({ firstName, middleName, lastName, studentNumber, course, email, password }, process.env.ACCOUNT_TOKEN, { expiresIn: process.env.REGISTER_EXPIRES });
 
     // create reset password url
-    const link = `${req.protocol}://${req.get('host')}/verify/account/${registerToken}`
+    const link = `$https://${req.get('host')}/verify/account/${registerToken}`
 
     try {
         const message = await verifyEmail({ link })
